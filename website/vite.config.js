438d44c4c0cfc856c1be4911c42cb4d41c861788
Code-God-2010 <linlagode@gmail.com>
2026-07-23 16:37:19 +0200

    fix things

--- website/vite.config.js
+++ website/vite.config.js
@@ -6,4 +6,5 @@ import cesium from "vite-plugin-cesium";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss(), cesium()],
+	base: "/Stratosphaerenballon/",
 });
