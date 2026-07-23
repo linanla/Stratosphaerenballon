ee9fb462433c0381c2d8ba7a4e04938b25812201
Code-God-2010 <linlagode@gmail.com>
2026-07-23 17:35:07 +0200

    fix scrolling

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -31,7 +31,7 @@ export default function App() {
 		return <div></div>;
 	}
 	return (
-		<div className="bg-gray-900 h-dvh flex flex-col gap-4">
+		<div className="bg-gray-900 h-dvh flex flex-col gap-4 overflow-auto">
 			<div>
         <Timeline
           max={gpsData[gpsData.length - 1].millis}
